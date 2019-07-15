19f62233eebea38a3305836efbd0ab48a80c6fe0
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-14 22:32:35 -0500

    express server wired up and making successful coinbase API calls

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/',(req,res)=>res.send("It's alive!!"))
+server.get('/', (req, res) => res.send("It's alive!!"))
 
 module.exports = server;
