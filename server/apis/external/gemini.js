5e83b27eb7599785dc6abc19bfc74a6618c72e51
WilliamConnatser <43946230+WilliamConnatser@users.noreply.github.com>
2019-09-04 19:37:38 -0500

    Gemini WS implementation to keep trades synced

--- server/apis/external/gemini.js
+++ server/apis/external/gemini.js
@@ -1,3 +1,4 @@
+const WebSocket = require('ws')
 const SmartAxios = require('../../utility/SmartAxios')
 const gemini = new SmartAxios('gemini')
 const objectToQuery = require('../../utility/objectToQuery')
@@ -94,10 +95,59 @@ function getAllTrades(tradingPair, timestamp) {
         // },
     //     ...
     // ]
-
 }
+
+function syncAllTrades(tradingPairs) {
+    tradingPairs.forEach(tradingPair => {
+        const queryParams = objectToQuery({trades: true})
+        //Setup WS
+        const ws = new WebSocket(`${process.env.GEMINI_WS}${tradingPair}${queryParams}`)
+        //Open WS connection
+        ws.on('open', () => {
+            console.log(`[GEMINI] WS Connected at ${process.env.GEMINI_WS}${tradingPair}${queryParams}`)
+            //No need for a subscription message
+            //Settings are handled via the WS path and query params
+        });
+    
+        //Handle messages received
+        ws.on('message', (data) => {
+            data = JSON.parse(data);
+            //Parse trade data from the message
+            const tradeData = data.events.map((trade) => {
+                return {
+                    time: new Date(data.timestampms),
+                    trade_id: trade.tid,
+                    price: trade.price,
+                    amount: trade.amount,
+                    exchange: 'gemini',
+                    trading_pair: tradingPair
+                }
+            })        
+            //Insert the parsed trade into the database
+            tradesApi.insert(tradeData)
+                .catch(err => {
+                    if(!err.message.includes('unique')) console.log(err.message, '<< GEMINI WS INSERTION')
+                })
+            //Update the console with the WS status
+            if (data.timestampms % process.env.UPDATE_FREQ === 0) {
+                console.log(`[GEMINI] WS ALIVE - ${new Date(data.timestampms)} - ${tradingPair}`)
+            }
+        });    
+        //Handle errors
+        ws.on('error', (err) => {
+            //TODO: Possible bug?
+            //Not sure why this 429 is occuring occasionally when the server first boots up
+            //Recursively call syncAllTrades again with only this trading pair to ensure full coverage
+            if(err.message.includes('429')) {
+                syncAllTrades([tradingPair])
+            } else {
+                console.log(err, '<< GEMINI WS', tradingPair)
+            }
+        })
+    })}
 
 module.exports = {
     getTradingPairs,
-    getAllTrades
+    getAllTrades,
+    syncAllTrades
 }
--- server/index.js
+++ server/index.js
@@ -12,57 +12,57 @@ const poloniex = require('./apis/external/poloniex')
 const idex = require('./apis/external/idex')
 const trades = require('./apis/db/trades')
 
-coinbase.getTradingPairs().then(res => {
-        //For Each Trading Pair
-        //Get All Trades VIA REST API
-        //Coinbase supports historical data
-        res.forEach(tradingPair => {
-            coinbase.getAllTrades(tradingPair)
-        })
-        //Keep up with all trades via WS communication
-        coinbase.syncAllTrades(res)
-    })
-    .catch(e => console.log(`[COINBASE] ERROR: ${e.message}`))
+// coinbase.getTradingPairs().then(res => {
+//         //For Each Trading Pair
+//         //Get All Trades VIA REST API
+//         //Coinbase supports historical data
+//         res.forEach(tradingPair => {
+//             coinbase.getAllTrades(tradingPair)
+//         })
+//         //Keep up with all trades via WS communication
+//         coinbase.syncAllTrades(res)
+//     })
+//     .catch(e => console.log(`[COINBASE] ERROR: ${e.message}`))
 
-bitstamp.getTradingPairs()
-    .then(res => {
-        //For Each Trading Pair
-        //Get All Trades VIA REST API
-        //Bitstamp does not support historical data
-        res.forEach(tradingPair => {
-            bitstamp.getAllTrades(tradingPair)
-        })
-        //Keep up with all trades via WS communication
-        bitstamp.syncAllTrades(res)
-    })
-    .catch(e => console.log(`[BITSTAMP] ERROR: ${e.message}`))
+// bitstamp.getTradingPairs()
+//     .then(res => {
+//         //For Each Trading Pair
+//         //Get All Trades VIA REST API
+//         //Bitstamp does not support historical data
+//         res.forEach(tradingPair => {
+//             bitstamp.getAllTrades(tradingPair)
+//         })
+//         //Keep up with all trades via WS communication
+//         bitstamp.syncAllTrades(res)
+//     })
+//     .catch(e => console.log(`[BITSTAMP] ERROR: ${e.message}`))
 
-bithumb.getTradingPairs()
-    .then(res => {
-        // For Each Trading Pair
-        // Get All Trades VIA REST API
-        // Bithumb does not support historical data
-        res.forEach(tradingPair => {
-            bithumb.getAllTrades(tradingPair)
-        })
-        //Bithumb does not appear to have WS
-    })
-    .catch(e => console.log(`[BITHUMB] ERROR: ${e.message}`))
+// bithumb.getTradingPairs()
+//     .then(res => {
+//         // For Each Trading Pair
+//         // Get All Trades VIA REST API
+//         // Bithumb does not support historical data
+//         res.forEach(tradingPair => {
+//             bithumb.getAllTrades(tradingPair)
+//         })
+//         //Bithumb does not appear to have WS
+//     })
+//     .catch(e => console.log(`[BITHUMB] ERROR: ${e.message}`))
 
-kraken.getTradingPairs()
-    .then(res => {
-        //For Each Trading Pair
-        //Get All Trades VIA REST API
-        //Kraken does not give historical data
-        //REST API does support longpolling
-        //Polls every 5 minutes
-        res.forEach(tradingPair => {
-            kraken.getAllTrades(tradingPair)
-        })
-        //Keep up with all trades via WS communication
-        kraken.syncAllTrades(res)
-    })
-    .catch(e => console.log(`[KRAKEN] ERROR: ${e.message}`))
+// kraken.getTradingPairs()
+//     .then(res => {
+//         //For Each Trading Pair
+//         //Get All Trades VIA REST API
+//         //Kraken does not give historical data
+//         //REST API does support longpolling
+//         //Polls every 5 minutes
+//         res.forEach(tradingPair => {
+//             kraken.getAllTrades(tradingPair)
+//         })
+//         //Keep up with all trades via WS communication
+//         kraken.syncAllTrades(res)
+//     })
+//     .catch(e => console.log(`[KRAKEN] ERROR: ${e.message}`))
     
 
 gemini.getTradingPairs()
@@ -70,10 +70,11 @@ gemini.getTradingPairs()
         //For Each Trading Pair
         //Get All Trades VIA REST API
         //Gemini only provides 7 days worth of historical data
-        res.forEach(tradingPair => {
-            gemini.getAllTrades(tradingPair)
-        })
-        //Todo: Implement Gemini WS
+        // res.forEach(tradingPair => {
+        //     gemini.getAllTrades(tradingPair)
+        // })
+        //Use Gemini WS to keep synced
+        gemini.syncAllTrades(res)
     })
     .catch(e => console.log(`[GEMINI] ERROR: ${e.message}`))
 
